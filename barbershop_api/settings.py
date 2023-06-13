68d824f39b885b3bc9000adffbf94f724764985d
dicodedao <dicodedao@gmail.com>
2023-06-13 11:17:47 +0700

    add allowed_hosts

--- barbershop_api/settings.py
+++ barbershop_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ckr3(zdsa*v+*y&absdelnc*kbtld6ybnp&r)(*ytiquv)-n_n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
